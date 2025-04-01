ba54558f2d04d37879ad546e0243bf03130ba09a
Lavale1012 <lavale889@gmail.com>
2025-04-01 17:41:37 -0400

    WIP: local changes before pull

--- Notes-and-Research/Backend-Test/supabase_test/config/supabaseClient.js
+++ Notes-and-Research/Backend-Test/supabase_test/config/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 
-dotenv.config(); // Load environment variables
+dotenv.config({ path: "./.env" }); // Load environment variables from .env file
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
